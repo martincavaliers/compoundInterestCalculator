1cb0a878a8bf5cd8c0f96ba663d98c401b195dbc
martincavaliers <martincoutts5@gmail.com>
2018-08-24 10:20:01 +0100

    calculate interest function not working properly, needs refinement

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const calculateButton = document.getElementById("calculateButton");
 const infoTable = document.getElementById("infoTable");
 yearsArray = new Array();
+interestArray = new Array();
+
+function calculateInterest(balance, deposit, interest){
+    let startBalance = balance + deposit;
+    let balanceInterest = (startBalance/100) * 100;
+    let newBalance = startBalance + interest;
+    return newBalance;
+}
 
 // Calculate Button Event Listener
 calculateButton.addEventListener('click', (e) => {
@@ -9,20 +17,22 @@ calculateButton.addEventListener('click', (e) => {
     let monthlyDeposit = document.getElementById("monthlyDeposit").value;
     let averageInterest = document.getElementById("averageInterest").value;
     let years = document.getElementById("years").value;
-    const calculateButton = document.getElementById("calculateButton");
 
     // Creating an array for years to use as id's for rows
     for(var i = 0; i < years; i++){
-        yearsArray.push(i);
+        yearsArray.push(i + 1);
     }
 
-    // for(var i = 0; i < years; i ++){
-    //     const tr = document.createElement("TR");
-    //     infoTable.appendChild(tr);
-    //     tr.setAttribute("id", i);
-    //     let row = document.getElementById(i);
-    //     let th = document.createElement("TH");
-    //     th.innerText = years;
-    //     i.appendChild(th);
-    // }
+    
+
+    for(var i = 0; i < years; i ++){
+        const tr = document.createElement("TR");
+        infoTable.appendChild(tr);
+        let rowNumber = yearsArray[i];
+        tr.setAttribute("id", rowNumber);
+        let row = document.getElementById(rowNumber);
+        let th = document.createElement("TH");
+        th.innerText = yearsArray[i];
+        row.appendChild(th);
+    }
 });
